cf402cdb6922b89ef9a39f21b9e39ec658d94e6b
olili27 <timmolili27@gmail.com>
2023-03-23 02:25:11 +0300

    finish bank account

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.Random;
+
 public class BankAccount {
 
     private String name;
@@ -7,25 +9,67 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        // initialize the attributes
 
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
+    // setter for balance
+    public void setBalance(double amount) {
+        this.balance = amount;
+    }
+
+    // getter for balance
+    public double getBalance() {
+        return this.balance;
+    }
+
+    // generate account number
     public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+        if(sum > 9 * digits) {
+            throw new Exception("Account Number can not be generated");
+        }
 
-        return null;
+        StringBuilder accountNumber = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < digits; i++) {
+            int digit;
+            if (i == digits - 1) {
+                digit = sum;
+            } else {
+                digit = random.nextInt(Math.min(9, sum) + 1);
+            }
+
+            accountNumber.append(digit);
+            sum -= digit;
+        }
+
+        return accountNumber.toString();
     }
 
+    // make a deposit
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
     }
 
+    // make a withdraw
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        try {
+            double newBalance = balance - amount;
 
+            if (newBalance < minBalance) throw new Exception();  // if balance is not enough
+
+            else {
+                balance = newBalance; // if balance is sufficient
+            }
+        } catch ( Exception e) {
+            System.out.println("Insufficient Balance");
+        }
     }
 
 }
