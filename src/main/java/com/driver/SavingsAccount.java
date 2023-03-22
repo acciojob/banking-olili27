7320d521863776c6c0e4d51d61a1068cc57762f4
olili27 <timmolili27@gmail.com>
2023-03-23 02:26:07 +0300

    finish savings account

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -5,24 +5,58 @@ public class SavingsAccount extends BankAccount{
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
-        // minimum balance is 0 by default
+        // initialize the attributes
 
+        super(name, balance, 0); // call the default bank account constructor
+
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
     }
+
+    // make withdraw
     public void withdraw(double amount) throws Exception {
-        // Might throw the following errors:
-        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
-        // 2. "Insufficient Balance" : If the amount exceeds balance
+        if (amount > this.getBalance()) { // if balance is not enough
+            throw new Exception("Insufficient Balance");
+        } else if (amount > this.maxWithdrawalLimit) { // if withdraw amount is beyond the limit
+            throw  new Exception("Maximum Withdraw Limit Exceed");
+        }
 
+        // successful withdraw
+        double currentBalance = this.getBalance();
+        currentBalance -= amount;
+
+        this.setBalance(currentBalance); // update the balance
     }
 
+    // calculate simple interest
     public double getSimpleInterest(int years){
-        // Return the final amount considering that bank gives simple interest on current amount
+        double interest = this.rate * this.getBalance();
+        double accumulatedInterest = interest * years;
 
+        double newBalance = this.getBalance() + accumulatedInterest;
+
+        this.setBalance(newBalance); // update the account balance
+
+        return newBalance;
     }
 
+    // calculate compound interest
     public double getCompoundInterest(int times, int years){
-        // Return the final amount considering that bank gives compound interest on current amount given times per year
+       for (int year = 1; year <= years; year++) {
 
+           while (times > 0) {
+               double currentBalance = this.getBalance();
+
+               double interest = rate * currentBalance;
+               double newBalance = currentBalance + interest;
+
+               this.setBalance(newBalance); // update the account balance
+
+               times--;
+           }
+       }
+
+       return this.getBalance();
     }
 
 }
